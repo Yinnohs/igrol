44c54951caf73287117076f8cfc94f1fee0d3e6a
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2024-11-18 16:29:00 +0000

    Feature :sparkles: (user): added to the controller update use cases

--- src/main/java/com/yinnohs/igrol/user/infrastructure/controller/UserController.java
+++ src/main/java/com/yinnohs/igrol/user/infrastructure/controller/UserController.java
@@ -3,6 +3,9 @@ package com.yinnohs.igrol.user.infrastructure.controller;
 import com.yinnohs.igrol.user.application.service.UserUseCases;
 import com.yinnohs.igrol.user.domain.model.User;
 import com.yinnohs.igrol.user.infrastructure.dto.CreateUserRequest;
+import com.yinnohs.igrol.user.infrastructure.dto.UpdateUserAddressRequest;
+import com.yinnohs.igrol.user.infrastructure.dto.UpdateUserEmailRequest;
+import com.yinnohs.igrol.user.infrastructure.dto.UpdateUserPhoneNumberRequest;
 import com.yinnohs.igrol.user.infrastructure.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +20,8 @@ public class UserController {
     private final UserMapper userMapper;
 
     @PostMapping()
-    public ResponseEntity<?> createUser(@RequestBody CreateUserRequest dto){
-        User userToCreate = userMapper.createDtoToUser(dto);
+    public ResponseEntity<?> createUser(@RequestBody CreateUserRequest request){
+        User userToCreate = userMapper.createDtoToUser(request);
 
         User user = userUseCases.create(userToCreate);
 
@@ -41,5 +44,20 @@ public class UserController {
         return ResponseEntity.ok("User Deleted successfully");
     }
 
-    @PutMapping("")
+    @PutMapping("/phone")
+    public ResponseEntity<?> updateUserPhoneNumber(@RequestBody UpdateUserPhoneNumberRequest request){
+        return ResponseEntity.ok(userUseCases.updateUserPhoneNumber(request.userId(), request.phoneNumber()));
+    }
+
+    @PutMapping("/email")
+    public ResponseEntity<?> updateUserEmail(@RequestBody UpdateUserEmailRequest request){
+        return ResponseEntity.ok(userUseCases.updateUserEmail(request.userId(), request.email()));
+    }
+
+    @PutMapping("/address")
+    public ResponseEntity<?> updateUserAddress(@RequestBody UpdateUserAddressRequest request){
+        return ResponseEntity.ok(userUseCases.updateUserAddress(request.userId(), request.userAddress()));
+    }
+
+
 }
