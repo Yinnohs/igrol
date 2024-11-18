745f6d7b8b87a4700c2deadb31d95e6200fce3e6
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2024-11-18 21:55:38 +0000

    Feature :sparkles: (product): added transforming / mapping functions to ProductDocument

--- src/main/java/com/yinnohs/igrol/product/infrastructure/document/ProductDocument.java
+++ src/main/java/com/yinnohs/igrol/product/infrastructure/document/ProductDocument.java
@@ -1,11 +1,13 @@
 package com.yinnohs.igrol.product.infrastructure.document;
 
+import com.yinnohs.igrol.product.domain.model.Product;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.validation.annotation.Validated;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,6 +19,7 @@ import java.time.LocalDateTime;
 @ToString
 @Builder
 @Document("product")
+@Validated
 public class ProductDocument {
     @Id
     private String id;
@@ -31,4 +34,26 @@ public class ProductDocument {
     private LocalDateTime createdAt;
     @LastModifiedDate
     private LocalDateTime lastUpdate;
+
+    public static ProductDocument fromProductModel(Product product){
+        return ProductDocument.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .imageUrl(product.getImageUrl())
+                .lastUpdate(product.getLastUpdate())
+                .createdAt(product.getCreatedAt())
+                .build();
+    }
+
+    public static Product toUserModel(ProductDocument product){
+        return Product.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .imageUrl(product.getImageUrl())
+                .lastUpdate(product.getLastUpdate())
+                .createdAt(product.getCreatedAt())
+                .build();
+    }
 }
