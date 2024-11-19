f89a2c3a01997b763a3170cbb2fa703ae15b214e
jisoto <jisoto@paradigmadigital.com>
2024-11-19 15:16:59 +0100

    Feature :sparkles: (auth): create AuthService

---
+++ src/main/java/com/yinnohs/igrol/auth/infrastructure/service/AuthService.java
@@ -0,0 +1,51 @@
+package com.yinnohs.igrol.auth.infrastructure.service;
+
+import com.yinnohs.igrol.user.domain.port.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AuthService {
+
+    private JwtEncoder jwtEncoder;
+    private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+
+    public String generateToken(Authentication authentication){
+        String scope = getScopeFromAuthentication(authentication);
+        var claims = getClaims("http//127.0.0.1:5052", authentication.getName(), scope);
+        return  jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    private String getScopeFromAuthentication(Authentication authentication){
+        return  authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+    }
+
+    private JwtClaimsSet getClaims(String issuer, String name, String scope){
+        var now = Instant.now();
+        return JwtClaimsSet.builder()
+                .issuer(issuer)
+                .issuedAt(now)
+                .subject(name)
+                .expiresAt(now.plus(1, ChronoUnit.DAYS))
+                .claim("scope", scope )
+                .build();
+
+    }
+}
