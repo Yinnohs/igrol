e64b0fe57ac0a1068f5492d8b8104c71b4e5abe9
jisoto <jisoto@paradigmadigital.com>
2024-11-19 16:02:40 +0100

    Feature :sparkles: (app): added support for rsa configs

--- src/main/java/com/yinnohs/igrol/IgrolApplication.java
+++ src/main/java/com/yinnohs/igrol/IgrolApplication.java
@@ -1,9 +1,12 @@
 package com.yinnohs.igrol;
 
+import com.yinnohs.igrol.config.RsaKeyConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyConfigProperties.class)
 public class IgrolApplication {
 
 	public static void main(String[] args) {
