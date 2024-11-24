122441b16350bdb24147f570ca63f81198b5687a
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2024-11-24 21:43:39 +0000

    fix :bug: (user): fixed bug where the test cases does not run for a duplicate column

--- src/test/java/com/yinnohs/igrol/user/ArchitectureUserPackageTest.java
+++ src/test/java/com/yinnohs/igrol/user/ArchitectureUserPackageTest.java
@@ -2,7 +2,7 @@ package com.yinnohs.igrol.user;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.lang.ArchRule;::
+import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
