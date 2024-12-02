2b8414b44f48f0422e3a596cd2c89a1c4ac4795d
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2024-12-02 22:45:28 +0000

    Feature :sparkles: (itemList): created bean config

---
+++ src/main/java/com/yinnohs/igrol/itemlist/infrastrucutre/controller/ItemListBeanConfiguration.java
@@ -0,0 +1,20 @@
+package com.yinnohs.igrol.itemlist.infrastrucutre.controller;
+
+import com.yinnohs.igrol.itemlist.aplication.usecases.impl.ItemListWhereUserIsParticipantUsesCasesImpl;
+import com.yinnohs.igrol.itemlist.domain.service.ItemListService;
+import com.yinnohs.igrol.user.domain.port.in.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ItemListBeanConfiguration {
+    private final ItemListService itemListService;
+    private final UserService userService;
+
+    @Bean
+    public ItemListWhereUserIsParticipantUsesCasesImpl itemListWhereUserIsParticipantUsesCasesImpl(){
+        return new ItemListWhereUserIsParticipantUsesCasesImpl(itemListService,userService);
+    }
+}
